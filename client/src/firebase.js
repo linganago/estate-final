cc724f32beeeabba4dff8c128c7ca05f9ef92818
linganago <linganagoudamaligouda@gmail.com>
2026-04-28 12:37:01 +0530

    firebase chnages

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 
-const envFirebaseConfig = {
+const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
@@ -9,32 +9,9 @@ const envFirebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-const fallbackFirebaseConfig = {
-  apiKey: 'replace-me',
-  authDomain: 'mern-estate-5f575.firebaseapp.com',
-  projectId: 'mern-estate-5f575',
-  storageBucket: 'mern-estate-5f575.firebasestorage.app',
-  messagingSenderId: '221547942417',
-  appId: '1:221547942417:web:6f81dd696d5280330417b0',
-};
-
-const providedEnvKeys = Object.entries(envFirebaseConfig)
-  .filter(([, value]) => Boolean(value))
-  .map(([key]) => key);
-const totalFirebaseConfigKeys = Object.keys(envFirebaseConfig).length;
-
-if (providedEnvKeys.length > 0 && providedEnvKeys.length < totalFirebaseConfigKeys) {
-  console.warn(
-    'Partial Firebase config detected. Use all Firebase web app values from the same project to avoid Google auth errors.'
-  );
+// Log in development to verify config is loaded
+if (import.meta.env.DEV) {
+  console.log('[Firebase] projectId:', firebaseConfig.projectId);
 }
-
-const firebaseConfig =
-  providedEnvKeys.length === totalFirebaseConfigKeys
-    ? envFirebaseConfig
-    : {
-        ...fallbackFirebaseConfig,
-        apiKey: envFirebaseConfig.apiKey,
-      };
 
 export const app = initializeApp(firebaseConfig);
