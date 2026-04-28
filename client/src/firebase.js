2cc29c4907dfddde52abd09b8d9ca24ba5dbb6f7
linganago <linganagoudamaligouda@gmail.com>
2026-04-28 15:39:21 +0530

    changed firebase file

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || 'AIzaSyBqbL2dKVNDH3rWFo42tiPRURJZDKGJ6A8',
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'mern-estate-aff00.firebaseapp.com',
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || 'mern-estate-aff00',
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || 'mern-estate-aff00.firebasestorage.app',
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '318830777936',
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || '1:318830777936:web:6f916386f0836cb36a6355',
 };
 
 export const app = initializeApp(firebaseConfig);
